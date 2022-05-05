0d1e3a5bd2a1354ca01456f5c8cb3b363c85ca98
Hacke <102891240+xhoho25@users.noreply.github.com>
2022-05-04 22:46:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -559,10 +559,10 @@ def onmessage(update,bot:ObigramClient):
 
 
 def main():
-    bot_token = os.environ.get('5389594317:AAFAEZCRGgSVbcyZLl9HH7MMKA9et2SVVGc')
+    bot_token = os.environ.get('5303780654:AAFVxgLSaPeOdDpbJEjak7YdnOArxAORbtU')
 
     #set in debug
-    bot_token = '5389594317:AAFAEZCRGgSVbcyZLl9HH7MMKA9et2SVVGc'
+    bot_token = '5303780654:AAFVxgLSaPeOdDpbJEjak7YdnOArxAORbtU'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
