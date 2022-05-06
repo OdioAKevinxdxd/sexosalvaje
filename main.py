ef25f53861d20a7475646a4600653cd27404415a
Hacke <102891240+xhoho25@users.noreply.github.com>
2022-05-06 14:53:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'Marlon9904'
+        tl_admin_user = 'MarlonSu'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
